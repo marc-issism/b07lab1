c016bb3c68f40b629364f158b947fd15fd1280f8
PizzaAuFromage <moojycua@gmail.com>
2024-05-30 10:59:44 -0400

    Modified Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -2,15 +2,48 @@ import java.math.*;
 
 public class Polynomial {
 
-    // Field
+    // Fields
     double[] coefficients;
+    int[] exponents;
 
+    // Given nothing, construct polynomial with coefficient 0 and exponent 1
     public Polynomial() {
-        coefficients = new double[]{0}; // Initialize to {0}
+        coefficients = new double[]{0}; // Initialize coefficient to {0}
+        exponents = new int[]{1}; // Initialize exponent to {1}
     }
 
+    // Given only coefficients, construct polynomial with each coefficient to the power of 1
     public Polynomial(double coefficients[]) {
         this.coefficients = coefficients; // Initialize to user given array of coefficients
+        exponents = new int[coefficients.length];
+        for (int i = 0; i < exponents.length; i++) {
+            exponents[i] = 1;
+        }
+    }
+
+    // Given both coefficients and exponents construct a polynomial with given coefficients and exponents
+    public Polynomial(double coefficients[], int exponents[]) {
+        this.coefficients = coefficients;
+
+        if (exponents.length > coefficients.length) { // Cut exponents down to how many coefficients there are
+            this.exponents = new int[coefficients.length];
+            for (int i = 0; i < coefficients.length; i++) {
+                this.exponents[i] = exponents[i];
+            }
+        }
+        else if (exponents.length < coefficients.length) { // Replace empty exponent indeces with 1s
+            this.exponents = new int[coefficients.length];
+            for (int i = 0; i < exponents.length; i++) {
+                this.exponents[i] = exponents[i];
+            }
+            for (int j = exponents.length; j < coefficients.length; j++) {
+                this.exponents[j] = 1;
+            }
+        }
+        else { // ex.length = co.length
+            this.exponents = exponents;
+        }
+
     }
 
     public Polynomial add(Polynomial polynomial) {
@@ -27,6 +60,18 @@ public class Polynomial {
 
         return new Polynomial(result_coefficients);
         
+    }
+
+    public String toString() {
+
+        String output = "";
+
+        for (int i = 0; i < coefficients.length; i++) {
+            output.concat(String.valueOf(coefficients[i]) + "x" );
+        } 
+
+        return output;
+
     }
 
     public double evaluate(double x) {
@@ -48,6 +93,20 @@ public class Polynomial {
         }
 
         return false;
+    }
+
+    public Polynomial multiply(Polynomial polynomial) {
+
+        int max_length = Math.max(polynomial.coefficients.length, coefficients.length);
+        int min_length = Math.min(polynomial.coefficients.length, coefficients.length);
+
+        //double result_coefficients 
+
+        Polynomial result = new Polynomial();
+
+
+
+        return result;
     }
 
 }
